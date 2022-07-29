8c42a2cf6b4d2feede8644a13710d714eb11d9c3
Jonathan Mendez <jwmp001@gmail.com>
2022-07-29 15:25:58 -0400

    Fixed Sidebar hover not appearing issue.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -25,14 +25,7 @@ const Sidebar = () => {
         <NavLink activeclassname="active" className="about-link" to="/about">
           <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
         </NavLink>
-        <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
-          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          activeclassname="active"
-          className="contact-link"
-          to="/contact"
-        >
+        <NavLink activeclassname="active" className="contact-link" to="/contact">
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
       </nav>
